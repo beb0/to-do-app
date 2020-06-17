5fea187cf27eba57f4b6006371b6ac431bab109e
abd-diaa <abdelrahman.diaa.g@gmail.com>
2020-06-17 20:09:32 +0200

    Basic Routing

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-var express = require('express')
-
-var app = express()
-
+http.createServer(function (req, res) {
+    console.log("Request was made at: " + req.url)
+    if  (req.url === "/home" || req.url === "/"){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        fs.createReadStream("home.html").pipe(res)
+    }
+    else if (req.url === "/contact"){
+        fs.createReadStream("contact.html").on('data', function (file) {
+            res.write(file)
+            res.end()
+        })
+    }
+    else if(req.url === "/api"){
+        fs.readFile("test.json", "utf8", function (err, data) {
+            res.writeHead(200,{'Content-Type': 'text/json'})
+            res.write(data)
+            res.end()
+        })
+    }
+    else{
+        var readStream = fs.createReadStream("notfound.html")
+        res.writeHead(404, {'Content-Type': 'text/html'})
+        readStream.pipe(res)
+    }
+}).listen(3000)
