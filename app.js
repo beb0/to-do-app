b960c9bce6604f094054840004e13e31f0a70484
abd-diaa <abdelrahman.diaa.g@gmail.com>
2020-06-22 00:58:11 +0200

    updated routing using Express,js

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
-var http = require('http')
-var fs = require('fs')
+var express = require('express')
 
-http.createServer(function (req, res) {
-    console.log("Request was made at: " + req.url)
-    if  (req.url === "/home" || req.url === "/"){
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        fs.createReadStream("home.html").pipe(res)
-    }
-    else if (req.url === "/contact"){
-        fs.createReadStream("contact.html").on('data', function (file) {
-            res.write(file)
-            res.end()
-        })
-    }
-    else if(req.url === "/api"){
-        fs.readFile("test.json", "utf8", function (err, data) {
-            res.writeHead(200,{'Content-Type': 'text/json'})
-            res.write(data)
-            res.end()
-        })
-    }
-    else{
-        var readStream = fs.createReadStream("notfound.html")
-        res.writeHead(404, {'Content-Type': 'text/html'})
-        readStream.pipe(res)
-    }
-}).listen(3000)
+var app = express()
+
+app.set('view engine', 'ejs')
+
+app.listen(3000)
+
+app.get('/', function (req, res) {
+    res.send("Heyyyyyy")
+})
+
+app.get('/home', function (req, res) {
+    res.sendFile(__dirname + "/home.html")
+})
+
+app.get('/contact/:name', function (req, res) {
+    // res.send("Contact us at: " + req.params.name) 
+    var data = {
+        num : 18, 
+        job: 'Software Developer',
+        hobbies: ['reading', 'listening to music']
+        }
+    res.render('contact', {name: req.params.name, num: 18, data:data})
+})
