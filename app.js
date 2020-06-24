d95d1af742d84eac7df9892ba62b406548bed583
abd-diaa <abdelrahman.diaa.g@gmail.com>
2020-06-24 03:37:44 +0200

    Testing some functionalities

--- app.js
+++ app.js
@@ -1,25 +1,55 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 
 var app = express()
 
-app.set('view engine', 'ejs')
+var jsonParser = bodyParser.json()
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.set('view engine', 'ejs')
+// app.use('/assets', function (req, res, next) {
+//     console.log(req.url)
+//     next()
+// })
+
+app.use('/assets', express.static('assets'))
 app.listen(3000)
 
 app.get('/', function (req, res) {
-    res.send("Heyyyyyy")
+    res.send("Please Enter url + /home")
 })
 
 app.get('/home', function (req, res) {
-    res.sendFile(__dirname + "/home.html")
+    res.render('home')
 })
 
 app.get('/contact/:name', function (req, res) {
     // res.send("Contact us at: " + req.params.name) 
+    res.render('contact_na', {name: req.params.name, num: 1800})
+})
+
+app.get('/contact', function (req, res) {
+    // res.send("Contact us at: " + req.params.name) 
+    console.log(req.query)
+    res.render('contact')
+})
+
+app.post('/contact', urlencodedParser, function (req, res) {
+    console.log(req.body)
+    res.render('success', {formd: req.body})
+})
+
+app.get('/profile/:name', function (req, res) {
     var data = {
-        num : 18, 
+        name : req.params.name, 
         job: 'Software Developer',
         hobbies: ['reading', 'listening to music']
         }
-    res.render('contact', {name: req.params.name, num: 18, data:data})
+
+    res.render('profile', {data:data})
 })
+
+// app.get('/profile', function(req,res){
+//     res.render('profile', {rq: req.query})
+// })
