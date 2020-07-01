7add1ef22bba9f5cde6eb2c8aa290e06017988a4
abd-diaa <abdelrahman.diaa.g@gmail.com>
2020-07-01 06:30:20 +0200

    Added Post and Delete functionality

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,14 +1,22 @@
+var data = [{item: 'test item'}]
+var bodyParser = require('body-parser')
+var urlencodedParser = bodyParser.urlencoded({extended: false})
+
 module.exports = function(app){
 
     app.get('/todo', function(req, res) {
-        res.render('todo')
+        res.render('todo', {items: data})
     });
 
-    app.post('/todo', function(req, res) {
-        
+    app.post('/todo', urlencodedParser, function(req, res) {
+        data.push(req.body)
+        res.json({items: data})
     });
 
-    app.delete('/todo', function(req, res) {
-        
+    app.delete('/todo/:item', function(req, res) {
+       data = data.filter(function (arr) {
+            return arr.item.replace(/ /g, '-') !== req.params.item
+        })
+        res.json(data)
     });
 }
